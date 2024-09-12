0fa5e0b36ef4de24e165ab2c28af8b0b21a52f36
katphamie <katnphmie@gmail.com>
2024-09-12 11:12:33 -0700

    weewooo

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,14 @@
-
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        double one = 32.6382;
+        double two = 64.3241;
+        System.out.println("Two original numbers:");
+        System.out.println(one);
+        System.out.println(two);
+        int roundone = (int) (one + 0.5);
+        int roundtwo = (int) (two + 0.5);
+        System.out.println("Answer: " + roundone + " + " + roundtwo + " = " + (roundone + roundtwo));
+    }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,13 @@
-
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        double num = 43.0924;
+        num *= 10;
+        System.out.println((int)(num%10));
+        num *= 10;
+        System.out.println((int)(num%10));
+        num *= 10;
+        System.out.println((int)(num%10));
+    }
+}
